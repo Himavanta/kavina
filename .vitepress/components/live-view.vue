bfd6834d60f58c0fea06dcf0a01f1c43f9f05a46
seven <monstime@foxmail.com>
2026-07-22 15:29:07 +0800

    feat: add live preview component to homepage

---
+++ .vitepress/components/live-view.vue
@@ -0,0 +1,22 @@
+<template>
+  <ClientOnly>
+    <Design :class="$style.Component" v-model:views="views" :widgets="packages" />
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import Design from "kavina/design";
+import * as example from "kavina/packages/example";
+import { ref } from "vue";
+
+const packages = Object.values(example);
+
+const views = ref([]);
+</script>
+
+<style lang="scss" module>
+.Component {
+  height: 60vw;
+  margin: 5vw 7vw;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,19 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
+import { h, computed } from "vue";
 import type { Theme } from "vitepress";
+import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import LiveView from "../components/live-view.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const { page } = useData();
+    const isHome = computed(() => page.value.relativePath === "index.md");
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      "home-hero-after": () => (isHome.value ? h(LiveView) : null),
     });
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  },
+  // enhanceApp({ app, router, siteData }) {},
 } satisfies Theme;
